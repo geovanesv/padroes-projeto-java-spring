b1be16fce2f2e06062304c54fd1bceac18568285
geovanesv <geovanesv89@gmail.com>
2024-08-01 20:34:35 -0300

    start padroes de projetos gof

---
+++ src/main/java/com/spring/gof/PadroesProjetoSprintBootApplication.java
@@ -0,0 +1,13 @@
+package com.spring.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesProjetoSprintBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesProjetoSprintBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/gof/PadroesProjetoSprintBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.gof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesProjetoSprintBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
