f07b29263032372081a0d3242d21a078df594efd
geovanesv <geovanesv89@gmail.com>
2024-08-01 21:51:28 -0300

    create interfaces services

---
+++ src/main/java/com/spring/gof/service/ClienteService.java
@@ -0,0 +1,25 @@
+package com.spring.gof.service;
+
+import com.spring.gof.model.Cliente;
+
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma
+ * interface.
+ *
+ *  Author Geovane Araujo
+ * */
+
+
+public interface ClienteService {
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
